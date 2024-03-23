78f3895b7fb0547a213d7408b0467fb46c1be3f4
Anton Agestam <git@antonagestam.se>
2024-03-23 12:57:12 +0100

    chore: Adjust docstrings to expose unions
    - Adding docstrings to some convenience union types to include them in
      docucmentation. Also adding docstrings to other protocols in the file
      for consistency.
    - Tune some formatting parameters for readability.

--- src/kio/static/protocol.py
+++ src/kio/static/protocol.py
@@ -36,11 +36,14 @@ RequestHeader: TypeAlias = (
     | kio.schema.request_header.v1.RequestHeader
     | kio.schema.request_header.v2.RequestHeader
 )
+"""Convenient union type of all possible request header types."""
 ResponseHeader: TypeAlias = (
     kio.schema.response_header.v0.ResponseHeader
     | kio.schema.response_header.v1.ResponseHeader
 )
+"""Convenient union type of all possible response header types."""
 Header: TypeAlias = RequestHeader | ResponseHeader
+"""Convenient union type of all possible request and response header types."""
 
 
 class Entity(DataclassInstance, Protocol):
@@ -93,28 +96,35 @@ class Payload(DataclassInstance, Protocol):
 
 
 class HeaderV0RequestPayload(Payload, Protocol):
+    """Protocol describing a request payload entity type with a V0 header."""
     __header_schema__: ClassVar[type[kio.schema.request_header.v0.RequestHeader]]
 
 
 class HeaderV1RequestPayload(Payload, Protocol):
+    """Protocol describing a request payload entity type with a V1 header."""
     __header_schema__: ClassVar[type[kio.schema.request_header.v1.RequestHeader]]
 
 
 class HeaderV2RequestPayload(Payload, Protocol):
+    """Protocol describing a request payload entity type with a V2 header."""
     __header_schema__: ClassVar[type[kio.schema.request_header.v2.RequestHeader]]
 
 
 RequestPayload: TypeAlias = (
     HeaderV0RequestPayload | HeaderV1RequestPayload | HeaderV2RequestPayload
 )
+"""Convenient union type of all possible request payloads."""
 
 
 class HeaderV0ResponsePayload(Payload, Protocol):
+    """Protocol describing a response payload entity type with a V0 header."""
     __header_schema__: ClassVar[type[kio.schema.response_header.v0.ResponseHeader]]
 
 
 class HeaderV1ResponsePayload(Payload, Protocol):
+    """Protocol describing a response payload entity type with a V1 header."""
     __header_schema__: ClassVar[type[kio.schema.response_header.v1.ResponseHeader]]
 
 
 ResponsePayload: TypeAlias = HeaderV0ResponsePayload | HeaderV1ResponsePayload
+"""Convenient union type of all possible response payloads."""
