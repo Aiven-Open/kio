Aiven-Open/kio

commit ccc757f52c0f6167946cbc72e7792419dd8828ff

feature: Implement dynamic schema entity imports (#157)
This commit implements a set of helper functions to dynamically import
entities from the schema. They allow:

- Importing requests given their response type, and vice versa.
- Importing payload entities given their API key and type.
- Importing any schema entitiy given its schema name and type.

This is backed by an index that maps (schema name, version, entity type)
to an importable path name. The index is pre-generated together with the
rest of the schema, to limit impact on runtime code as much as possible.

Supersedes #150, fixes #115.

codegen/introspect_schema.py
@@ -1,0 +1,53 @@
+from collections.abc import Iterator
+from importlib import import_module
+from pathlib import Path
+from pkgutil import walk_packages
+from types import ModuleType
+from typing import Final
+
+import kio.schema
+
+from kio.static.constants import EntityType
+from kio.static.protocol import Entity
+
+base_dir: Final = Path(__file__).parent.parent.resolve()
+schema_src_dir: Final = Path(kio.schema.__file__).parent.resolve()
+ignore_modules: Final = frozenset({"index"})
+
+
+def generate_modules(parent: ModuleType) -> Iterator[ModuleType]:
+    for package in walk_packages(parent.__path__):
+        if package.name in ignore_modules:
+            continue
+        module = import_module(f"{parent.__name__}.{package.name}")
+        if package.ispkg:
+            yield from generate_modules(module)
+        else:
+            yield module
+
+
+def get_entities() -> Iterator[tuple[type[Entity], str]]:
+    modules = list(generate_modules(import_module("kio.schema")))
+    for module in modules:
+        items = module.__dict__.copy()
+        # Eliminate non-entity modules, situated directly under `kio.schema`.
+        if sum(1 for c in module.__name__ if c == ".") < 3:
+            continue
+        for key, value in items.items():
+            if key.startswith("__"):
+                continue
+            # Eliminate anything not defined in the module.
+            if getattr(value, "__module__", None) != module.__name__:
+                continue
+            # Eliminate anything that's not a class.
+            if type(value) is not type:
+                continue
+            assert isinstance(module.__file__, str)
+            yield value, module.__file__
+
+
+def get_message_entities() -> Iterator[tuple[type[Entity], str]]:
+    for entity, file_path in get_entities():
+        if entity.__type__ is EntityType.nested:
+            continue
+        yield entity, file_path

codegen/__main__.py
@@ -1,6 +1,8 @@
+from . import generate_index
 from . import generate_schema
 from . import generate_tests
 
 if __name__ == "__main__":
     generate_schema.main()
     generate_tests.main()
+    generate_index.main()

codegen/generate_index.py
@@ -1,0 +1,160 @@
+# ruff: noqa: T201
+
+import sys
+
+from collections import defaultdict
+from collections.abc import Iterator
+from collections.abc import Mapping
+from functools import partial
+from typing import Final
+from typing import NamedTuple
+from typing import TypeAlias
+
+from kio.static.constants import EntityType
+from kio.static.protocol import Entity
+
+from .introspect_schema import base_dir
+from .introspect_schema import get_message_entities
+
+target_path: Final = base_dir / "src/kio/schema/index.py"
+prefix: Final = "kio.schema."
+indent: Final = "    "
+
+module_setup: Final = """\
+from collections.abc import Mapping
+from types import MappingProxyType
+from typing import Final
+from typing import Literal
+from typing import TypeAlias
+
+from kio.static.constants import EntityType
+
+__all__ = (
+    "api_key_map",
+    "schema_name_map",
+    "PayloadEntityType",
+    "LoadableEntityType",
+)
+
+PayloadEntityType: TypeAlias = Literal[EntityType.response, EntityType.request]
+LoadableEntityType: TypeAlias = (
+    Literal[EntityType.header, EntityType.data] | PayloadEntityType
+)
+TypeMap: TypeAlias = Mapping[LoadableEntityType, str]
+VersionMap: TypeAlias = Mapping[int, TypeMap]
+SchemaNameMap: TypeAlias = Mapping[str, VersionMap]
+APIKeyMap: TypeAlias = Mapping[int, str]
+
+"""
+
+
+class PathParts(NamedTuple):
+    api_name: str
+    version: int
+    entity_type: EntityType
+    entity_name: str
+
+
+def module_path_parts(entity: type[Entity]) -> PathParts:
+    without_prefix = entity.__module__.removeprefix(prefix)
+    parts = without_prefix.split(".")
+    assert parts[-1] == entity.__type__.name
+    return PathParts(
+        api_name=parts[0],
+        version=int(parts[1][1:]),
+        entity_type=entity.__type__,
+        entity_name=entity.__qualname__,
+    )
+
+
+def fqn(parts: PathParts) -> str:
+    return (
+        f"{prefix}"
+        f"{parts.api_name}"
+        f".v{parts.version}"
+        f".{parts.entity_type.name}"
+        f":{parts.entity_name}"
+    )
+
+
+TypeMap: TypeAlias = Mapping[EntityType, str]
+VersionMap: TypeAlias = Mapping[int, TypeMap]
+SchemaNameMap: TypeAlias = Mapping[str, VersionMap]
+APIKeyMap: TypeAlias = Mapping[int, str]
+
+
+def build_index() -> tuple[SchemaNameMap, APIKeyMap]:
+    schema_name_map: defaultdict[str, defaultdict[int, dict[EntityType, str]]] = (
+        defaultdict(partial(defaultdict, dict))
+    )
+    api_key_map = {}
+    for entity, _ in get_message_entities():
+        parts = module_path_parts(entity)
+        schema_name_map[parts.api_name][parts.version][parts.entity_type] = fqn(parts)
+        if (api_key := getattr(entity, "__api_key__", None)) is not None:
+            api_key_map[api_key] = parts.api_name
+    api_key_map = dict(sorted(api_key_map.items()))
+    return schema_name_map, api_key_map
+
+
+def format_schema_name_map(index: SchemaNameMap) -> Iterator[str]:
+    yield "schema_name_map: Final[SchemaNameMap] = MappingProxyType({"
+
+    for schema_name, version_map in index.items():
+        yield f'{indent}"{schema_name}": MappingProxyType({{'
+
+        for version, type_map in version_map.items():
+            yield f"{indent}{indent}{version}: MappingProxyType({{"
+
+            for entity_type, entity_path in type_map.items():
+                yield (
+                    f"{indent}{indent}{indent}EntityType.{entity_type.name}: ("
+                    f'"{entity_path}"),'
+                )
+
+            yield f"{indent}{indent}}}),"
+
+        yield f"{indent}}}),"
+
+    yield "})"
+    yield ""
+
+
+def format_api_key_map(index: APIKeyMap) -> Iterator[str]:
+    yield "api_key_map: Final[APIKeyMap] = MappingProxyType({"
+
+    for api_key, module_name in index.items():
+        yield f'{indent}{api_key}: "{module_name}",'
+
+    yield "})"
+    yield ""
+
+
+def main() -> None:
+    print("Generating index.", file=sys.stderr)
+    print("Introspecting modules ...", file=sys.stderr, flush=True)
+    schema_name_map, api_key_map = build_index()
+
+    # Smoke test
+    assert (
+        schema_name_map["metadata"][11][EntityType.request]
+        == "kio.schema.metadata.v11.request:MetadataRequest"
+    )
+    assert api_key_map[3] == "metadata"
+
+    print("Writing index file ...", file=sys.stderr, flush=True)
+
+    with target_path.open("w") as target:
+        print(module_setup, file=target)
+
+        for line in format_api_key_map(api_key_map):
+            print(line, file=target)
+
+        for line in format_schema_name_map(schema_name_map):
+            print(line, file=target)
+
+    print("Done.", file=sys.stderr, flush=True)
+
+
+if __name__ == "__main__":
+    main()

codegen/generate_tests.py
@@ -1,50 +1,19 @@
+# ruff: noqa: T201
+
 import os
 import shutil
+import sys
 
 from collections import defaultdict
-from collections.abc import Iterator
-from importlib import import_module
 from itertools import chain
 from pathlib import Path
-from pkgutil import walk_packages
-from types import ModuleType
-
-import kio.schema
 
 from kio.static.constants import EntityType
-from kio.static.protocol import Entity
 
 from .case import to_snake_case
-
-
-def generate_modules(parent: ModuleType) -> Iterator[ModuleType]:
-    for package in walk_packages(parent.__path__):
-        module = import_module(f"{parent.__name__}.{package.name}")
-        if package.ispkg:
-            yield from generate_modules(module)
-        else:
-            yield module
-
-
-def get_entities() -> Iterator[tuple[type[Entity], str]]:
-    modules = list(generate_modules(import_module("kio.schema")))
-    for module in modules:
-        items = module.__dict__.copy()
-        # Eliminate non-entity modules, situated directly under `kio.schema`.
-        if sum(1 for c in module.__name__ if c == ".") < 3:
-            continue
-        for key, value in items.items():
-            if key.startswith("__"):
-                continue
-            # Eliminate anything not defined in the module.
-            if getattr(value, "__module__", None) != module.__name__:
-                continue
-            # Eliminate anything that's not a class.
-            if type(value) is not type:
-                continue
-            assert isinstance(module.__file__, str)
-            yield value, module.__file__
-
+from .introspect_schema import base_dir
+from .introspect_schema import get_entities
+from .introspect_schema import schema_src_dir
 
 imports = """\
 from __future__ import annotations
@@ -85,9 +54,7 @@ def test_{entity_snake_case}_java(instance: {entity_type}, java_tester: JavaTest
 """
 
 
-base_dir = Path(__file__).parent.parent.resolve()
 generated_tests_module = base_dir / "tests" / "generated"
-schema_src_dir = Path(kio.schema.__file__).parent.resolve()
 
 
 def build_filename(source_path: str) -> str:
@@ -97,6 +64,7 @@ def build_filename(source_path: str) -> str:
 
 
 def main() -> None:
+    print("Generating tests.", file=sys.stderr)
     shutil.rmtree(generated_tests_module, ignore_errors=True)
     os.mkdir(generated_tests_module)
     (generated_tests_module / "__init__.py").touch()

pyproject.toml
@@ -147,6 +147,7 @@ max-complexity = 10
 [tool.ruff.lint.flake8-tidy-imports]
 ban-relative-imports = "parents"
 
+
 [tool.importlinter]
 root_packages = ["kio", "codegen", "tests"]
 

src/kio/index.py
@@ -1,0 +1,168 @@
+from pkgutil import resolve_name
+from types import ModuleType
+from typing import cast
+
+from .schema.index import LoadableEntityType
+from .schema.index import PayloadEntityType
+from .schema.index import api_key_map
+from .schema.index import schema_name_map
+from .static.constants import EntityType
+from .static.protocol import Entity
+from .static.protocol import RequestPayload
+from .static.protocol import ResponsePayload
+
+
+class KioIndexError(RuntimeError): ...
+
+
+class UnknownAPIKey(KioIndexError): ...
+
+
+class UnknownEntity(KioIndexError): ...
+
+
+def _name_from_key(api_key: int) -> str:
+    """
+    :raises UnknownAPIKey: When the given API key does not map to an API name.
+    """
+    try:
+        return api_key_map[api_key]
+    except KeyError as e:
+        raise UnknownAPIKey(f"Failed mapping the given API key: {api_key!r}") from e
+
+
+def _get_entity_path(name: str, version: int, entity_type: LoadableEntityType) -> str:
+    """
+    :raises UnknownEntity: When the given arguments do not map to a known entity.
+    """
+    try:
+        return schema_name_map[name][version][entity_type]
+    except KeyError:
+        raise UnknownEntity(
+            f"Failed mapping to a an entity path for ({name=!r}, {version=!r}, "
+            f"entity_type={entity_type.name!r})."
+        ) from None
+
+
+def _resolve(path: str) -> object:
+    try:
+        return resolve_name(path)
+    except (ImportError, ValueError, AttributeError) as e:
+        e.add_note("This error stems from a dynamic import in kio.")
+        raise
+
+
+def load_entity_module(
+    name: str,
+    version: int,
+    entity_type: LoadableEntityType,
+) -> ModuleType:
+    """
+    Load a schema module given the schema name, version, and type.
+
+    :raises UnknownEntity: When the given arguments do not map to a known entity.
+
+    >>> load_entity_module("metadata", 12, EntityType.request)
+    <module 'kio.schema.metadata.v12.request' from '.../kio/schema/metadata/v12/request.py'>
+    """
+    entity_path = _get_entity_path(name, version, entity_type)
+    module_path = entity_path.split(":", 1)[0]
+    resolved = _resolve(module_path)
+    assert isinstance(resolved, ModuleType)
+    return resolved
+
+
+def load_payload_module(
+    api_key: int,
+    version: int,
+    entity_type: PayloadEntityType,
+) -> ModuleType:
+    """
+    Load a schema module of an API message payload, given the API key, version,
+    and type.
+
+    :raises UnknownAPIKey: When the given API key does not map to an API name.
+    :raises UnknownEntity: When the given arguments do not map to a known entity.
+
+    >>> load_payload_module(3, 12, EntityType.request)
+    <module 'kio.schema.metadata.v12.request' from '.../kio/schema/metadata/v12/request.py'>
+    """
+    return load_entity_module(_name_from_key(api_key), version, entity_type)
+
+
+def load_entity_schema(
+    name: str,
+    version: int,
+    entity_type: LoadableEntityType,
+) -> type[Entity]:
+    """
+    Load an entity schema module given the schema name, version, and type.
+
+    :raises UnknownEntity: When the given arguments do not map to a known entity.
+
+    >>> load_entity_schema("metadata", 12, EntityType.response)
+    <class 'kio.schema.metadata.v12.response.MetadataResponse'>
+    """
+    resolved = _resolve(_get_entity_path(name, version, entity_type))
+    assert isinstance(resolved, type)
+    return cast(type[Entity], resolved)
+
+
+def load_response_schema(api_key: int, version: int) -> type[ResponsePayload]:
+    """
+    Load a response payload schema entity given its API key, and version.
+
+    :raises UnknownAPIKey: When the given API key does not map to an API name.
+    :raises UnknownEntity: When the given arguments do not map to a known entity.
+
+    >>> load_response_schema(3, 12)
+    <class 'kio.schema.metadata.v12.response.MetadataResponse'>
+    """
+    resolved = load_entity_schema(_name_from_key(api_key), version, EntityType.response)
+    return cast(type[ResponsePayload], resolved)
+
+
+def load_request_schema(api_key: int, version: int) -> type[RequestPayload]:
+    """
+    Load a request payload schema entity given its API key, and version.
+
+    :raises UnknownAPIKey: When the given API key does not map to an API name.
+    :raises UnknownEntity: When the given arguments do not map to a known entity.
+
+    >>> load_request_schema(3, 12)
+    <class 'kio.schema.metadata.v12.request.MetadataRequest'>
+    """
+    resolved = load_entity_schema(_name_from_key(api_key), version, EntityType.request)
+    return cast(type[RequestPayload], resolved)
+
+
+def load_response_from_request(
+    request_type: type[RequestPayload] | RequestPayload,
+) -> type[ResponsePayload]:
+    """
+    Load a response payload schema entity given its corresponding request type.
+
+    :raises UnknownAPIKey: When the given API key does not map to an API name.
+    :raises UnknownEntity: When the given arguments do not map to a known entity.
+
+    >>> from kio.schema.metadata.v12 import MetadataRequest
+    >>> load_response_from_request(MetadataRequest)
+    <class 'kio.schema.metadata.v12.response.MetadataResponse'>
+    """
+    return load_response_schema(request_type.__api_key__, request_type.__version__)
+
+
+def load_request_from_response(
+    response_type: type[ResponsePayload] | ResponsePayload,
+) -> type[RequestPayload]:
+    """
+    Load a request payload schema entity given its corresponding response type.
+
+    :raises UnknownAPIKey: When the given API key does not map to an API name.
+    :raises UnknownEntity: When the given arguments do not map to a known entity.
+
+    >>> from kio.schema.metadata.v12 import MetadataResponse
+    >>> load_request_from_response(MetadataResponse)
+    <class 'kio.schema.metadata.v12.request.MetadataRequest'>
+    """
+    return load_request_schema(response_type.__api_key__, response_type.__version__)

tests/schema/test_index.py
@@ -1,0 +1,18 @@
+from kio.schema.index import api_key_map
+from kio.schema.index import schema_name_map
+from kio.static.constants import EntityType
+
+
+def test_smoke_test_api_key_map() -> None:
+    assert api_key_map[3] == "metadata"
+
+
+def test_smoke_test_schema_name_map() -> None:
+    assert (
+        schema_name_map["metadata"][12][EntityType.request]
+        == "kio.schema.metadata.v12.request:MetadataRequest"
+    )
+    assert (
+        schema_name_map["metadata"][12][EntityType.response]
+        == "kio.schema.metadata.v12.response:MetadataResponse"
+    )

commit 73abcbfcee77eceefc43ed3cd5c22ffb8d165c8f

chore: Simplify module depth count expression (#163)
Follow-up item broken out from #157.

codegen/introspect_schema.py
@@ -31,7 +31,7 @@ def get_entities() -> Iterator[tuple[type[Entity], str]]:
     for module in modules:
         items = module.__dict__.copy()
         # Eliminate non-entity modules, situated directly under `kio.schema`.
-        if sum(1 for c in module.__name__ if c == ".") < 3:
+        if module.__name__.count(".") < 3:
             continue
         for key, value in items.items():
             if key.startswith("__"):